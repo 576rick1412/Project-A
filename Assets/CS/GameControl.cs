771f46f5b1bcbbc481b9fa8d572f6e213ac9f4ee
576rick1412 <576rick1412@gmail.com>
2022-11-30 23:50:13 +0900

    스크립트 개편
    애니메이션으로 작동하던 회전을 스크립트로 대체함
    스크립트 정리

--- Assets/CS/GameControl.cs
+++ Assets/CS/GameControl.cs
@@ -18,12 +18,24 @@ public class GameControl : MonoBehaviour
 
     [SerializeField] Card_Info[] card_Info;
 
-    bool Combineing = true;  // 카드 섞는 중
-    int  Combine_num = 0; // 카드 섞은 횟수
+    [SerializeField] float RotationSpeed;   // 카드 회전 속도
+    bool  NextRound = true;     // 다음 라운드로 이동 / 참 : 동작 , 거짓 : 중단
+    bool Combineing = true;     // 카드 섞는 중
+    int  Combine_num = 0;       // 카드 섞은 횟수
 
     void Start()
     {
-        // 카드 초기화
+        Init_Struct(); // 카드 초기화
+        StartCoroutine(Rotation_Card(true));
+    }
+
+    void Update()
+    {
+        Combine();
+    }
+
+    void Init_Struct()
+    {
         System.Array.Resize(ref card_Info, card_Info.Length + Card.Length);
         for (int i = 0; i < Card.Length; i++)
         {
@@ -32,14 +44,14 @@ public class GameControl : MonoBehaviour
             card_Info[i].Card_Check = true;
             card_Info[i].Card_Value = 0;
         }
-    }
+    } // 카드 초기화
 
-    void Update()
+    void Combine()
     {
-        // 카드 섞기 시작
-        int Combine_times = GameManager.GM.Data.Combine_times;
+        if (!NextRound) return; // 다음 라운드가 거짓일 때 리턴
 
-        if(Combine_num < Combine_times && Combineing)
+        int Combine_times = GameManager.GM.Data.Combine_times;
+        if (Combine_num < Combine_times && Combineing)
         {
             int a = Random.Range(0, Card.Length);
             int b = Random.Range(0, Card.Length);
@@ -47,12 +59,25 @@ public class GameControl : MonoBehaviour
 
             StartCoroutine(Combine_Card(a, b));
         }
-    }
+    } // 카드 섞기 시작
+
+    IEnumerator Rotation_Card(bool Side)
+    {
+        // Side가 참일 때 앞면 -> 뒷면 / Side가 거짓일 때 뒷면 -> 앞면
+        Vector3 Vec = Side == true ? new Vector3(0f, 180f, 0f) : new Vector3(0f, 0f, 0f);
+
+        for (int i = 0; i < Card.Length;i++)
+        {
+            Card[i].transform.DORotate(Vec, RotationSpeed).SetEase(Ease.InOutQuad);
+            yield return new WaitForSeconds(0.2f);
+        }
+        NextRound = true; // 다음 라운드로 돌아가도록 참으로 변경
+        yield return null;
+    } // 카드 회전
+
     IEnumerator Combine_Card(int Card_1,int Card_2)
     {
         Combineing = false; // if 문이 돌아가지 않도록 거짓으로 변경
-
-        GameObject Temp;
 
         // 카드 이동
         float Combine_Speed = GameManager.GM.Data.Combine_Speed;
@@ -61,17 +86,19 @@ public class GameControl : MonoBehaviour
         yield return new WaitForSeconds(Combine_Speed * 2);
 
         // 카드 위치 교환
-        Temp = Card[Card_1];
-        Card[Card_1] = Card[Card_2];
-        Card[Card_2] = Temp;
+        GameObject Temp = Card[Card_1];
+        Card[Card_1]    = Card[Card_2];
+        Card[Card_2]    = Temp;
 
-        Struct_Reset();
+        Reset_Struct();
 
         Combineing = true; // if 문이 돌아가도록 참으로 변경
+        NextRound = false; // 카드 선택 때까지 섞기 중단
         Combine_num++; // 섞은 횟수 1 추가
         yield return null;
     } // 카드 섞기 코루틴
-    void Struct_Reset()
+
+    void Reset_Struct()
     {
         for (int i = 0; i < Card.Length; i++)
         {
@@ -79,14 +106,4 @@ public class GameControl : MonoBehaviour
             card_Info[i].Card_anim = Card[i].GetComponent<Animator>();
         }
     } // 구조체 초기화 / 카드가 섞일 때 재정렬 하기 위함
-
-    public void Rotate_Card(int Value)
-    {
-        bool Card_Check = card_Info[Value].Card_Check;
-
-        card_Info[Value].Card_Value = Card_Check == true ? 2 : 1;
-        card_Info[Value].Card_Check = !Card_Check;
-
-        card_Info[Value].Card_anim.SetInteger("OnCard", card_Info[Value].Card_Value);
-    } // 카드 돌리기
 }
